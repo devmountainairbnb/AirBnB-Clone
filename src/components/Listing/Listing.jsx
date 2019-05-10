0b816c9e624b39a57142cd1c23a61d8b5edc2f74
Micaiah Buttars <michael.buttars@gmail.com>
2019-05-10 11:21:03 -0600

    first commit from branch

---
+++ src/components/Listing/Listing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Listing(){
+    return (
+        <div>
+            <h1>Listing</h1>
+            <span>header</span>
+            <div>
+                Images
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default Listing
