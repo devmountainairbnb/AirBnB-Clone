a4441585a68dea9aca7609d8de21f948a2eafde7
Spencer <sdvair@gmail.com>
2019-05-10 11:15:01 -0600

    Progress on the frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+//IMPORTED COMPONENTS
+import Home from './components/Home/Home.jsx'
+import FilteredHomes from './components/FilteredHomes/FilteredHomes.jsx'
+import UnfilteredHomes from './components/UnfilteredHomes/UnfilteredHomes.jsx'
+import Listing from './components/Listing/Listing'
+import NewProperty from './components/NewProperty/NewProperty.jsx'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/filteredHomes" component={FilteredHomes} />
+          <Route path="/unfilteredHomes" component={UnfilteredHomes} />
+          <Route path="/listing" component={Listing} />
+          <Route path="/newProperty" component={NewProperty} />
+        </Switch>
+      </HashRouter>
+    </div>
   );
 }
 
---
+++ src/components/UnfilteredHomes/UnfilteredHomes.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UnfilteredHomes extends Component {
+    render() {
+        return (
+            <div>UnfilteredHomes Component</div>
+        )
+    }
+}
+
+export default UnfilteredHomes
