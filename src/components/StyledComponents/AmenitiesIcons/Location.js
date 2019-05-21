72bfded8b3489210d4f0a80f6f18ea6374359923
Micaiah Buttars <michael.buttars@gmail.com>
2019-05-21 09:28:44 -0600

    progress on booking functionality

---
+++ src/components/StyledComponents/AmenitiesIcons/Location.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SVG = ({
+  style = {},
+  color = "#333",
+  width = "100%",
+  className = "",
+  viewBox="0 0 24 24"
+}) => (
+  <svg
+    width={width}
+    style={style}
+    height={width}
+    viewBox={viewBox}
+    xmlns="http://www.w3.org/2000/svg"
+    className={`svg-icon ${className || ""}`}
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+        <path stroke={color} fill={color} d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+
+  </svg>
+);
+
+export default SVG;
